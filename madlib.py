a38e6277a248538773fd2cf5172a92cfdaf9e34d
Kusuma Venkata Ramani Kusampudi <107961606+Kusuma586@users.noreply.github.com>
2022-06-28 00:19:09 -0500

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,16 @@
+Name_a_person=input("Name_a_person: ")
+verb1 = input("name your favorite job: ")
+Name_a_place=input("enter name of a place: ")
+name_a_place=input("enter another name of a place: ")
+Name_a_Place=input("enter another name of a place: ")
+write_moral_of_the_story =input("Give your own moral to the story: ")
+
+
+
+
+madlib = f"Hey there! I would like to tell a story about {Name_a_person}!\
+    This person wants to become a {verb1},Very passionatly learnt all the related things that are trending now a  days and \
+    and very postive toward the goal,Started attending interviews. 1st interview is at {Name_a_place}, got rejected. 2nd interview is at {name_a_place}, got rejected. 3rd interview is at {Name_a_Place}, got rejected😓.But he/she didnt stoped his/her trails \
+    He/She then decided to know his/her streantghts and weekness, sharpen their skills and tried again and again untill he/she gets success.Finally got his/her dream job💃💃.\
+    {write_moral_of_the_story}"
+print(madlib)
